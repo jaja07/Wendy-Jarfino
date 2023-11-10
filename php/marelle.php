17c0ff4861aba7f61ac05f5a2d2aee9263de9954
WendyYelena <wendynkouyelena@gmail.com>
2023-11-10 14:19:23 +0100

    1er commit de Wendy

---
+++ php/marelle.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    $titre = "Marelle";
+    include 'header.inc.php';
+    include 'menu.inc.php';
+
+?>
+
+<h1>ESIG'GAMES</h1>
+        <p>
+            <img class="left" 
+             src="photos\marelle.PNG" width="500" height="300" 
+             alt="cartes" title="" style="float:left;margin:70px 10px 50px 20px;" />
+            
+        </p>
+
+        <div >
+        <p  style="float:left;margin:70px 10px 50px 20px;">
+        <strong>Nom: </strong>Marelle<br/>
+        <strong>Description: </strong><br/>
+        Les Marelles, bien qu’homonymes, n’ont que peu de chose à voir avec le Jeu de Marelle qui se joue en extérieur à cloche-pied.
+       <br/> Il existe différentes variantes de ce jeu.
+       <br/> Le damier peut différer présentant plus ou moins de possibilités de placements. 
+       <br/> Souvent, plus le damier propose d’emplacements, plus le nombre de pions que possèdent les joueurs est important.
+       <br/><br/><strong>Règles détaillées: </strong> <a href="règles\La_règle_de_la_Marelle.pdf" target="_blank">Télécharger le PDF</a>
+            
+        </p>
